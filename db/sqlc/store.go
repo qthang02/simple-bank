31dd88372f200c08bd4c37c0f910ce2ebec5b460
qthang02 <nguyenquocthang909@gmail.com>
2023-06-26 22:01:42 +0700

    Send async tasks to Redis within a DB transaction

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -8,6 +8,7 @@ import (
 type Store interface {
 	Querier
 	TransferTx(cxt context.Context, arg TransferTxParams) (TransferTxResult, error)
+ 	CreateUserTx(cxt context.Context, arg CreateUserTxParams) (CreateUserTxResult, error) 
 }
 
 type SQLStore struct {
@@ -43,92 +44,4 @@ func (store *SQLStore) execTx(cxt context.Context, fn func(*Queries) error) erro
 	}
 	// Commit the transaction if there is no error
 	return tx.Commit()
-}
-
-type TransferTxParams struct {
-	FromAccountID int64 `json:"from_account_id"`
-	ToAccountID   int64 `json:"to_account_id"`
-	Amount        int64 `json:"amount"`
-}
-
-type TransferTxResult struct {
-	Transfer    Transfer `json:"transfer"`
-	FromAccount Account  `json:"from_account"`
-	ToAccount   Account  `json:"to_account"`
-	FromEntry   Entry    `json:"from_entry"`
-	ToEntry     Entry    `json:"to_entry"`
-}
-
-// TransferTx performs a money transfer from one account to the other.
-// It creates a transfer record, and account entries, and update accounts' balance within a single database transaction.
-
-func (store *SQLStore) TransferTx(cxt context.Context, arg TransferTxParams) (TransferTxResult, error) {
-	var result TransferTxResult
-
-	err := store.execTx(cxt, func(q *Queries) error {
-		var err error
-
-		result.Transfer, err = q.CreateTransfer(cxt, CreateTransferParams{
-			FromAccountID: arg.FromAccountID,
-			ToAccountID:   arg.ToAccountID,
-			Amount:        arg.Amount,
-		})
-
-		if err != nil {
-			return err
-		}
-
-		result.FromEntry, err = q.CreateEntry(cxt, CreateEntryParams{
-			AccountID: arg.FromAccountID,
-			Amount:    -arg.Amount,
-		})
-
-		if err != nil {
-			return err
-		}
-
-		result.ToEntry, err = q.CreateEntry(cxt, CreateEntryParams{
-			AccountID: arg.ToAccountID,
-			Amount:    arg.Amount,
-		})
-
-		if err != nil {
-			return err
-		}
-
-		// get account -> update account balance
-		if arg.FromAccountID < arg.ToAccountID {
-			result.FromAccount, result.ToAccount, err = addMoney(cxt, q, arg.FromAccountID, -arg.Amount, arg.ToAccountID, arg.Amount)
-		} else {
-			result.ToAccount, result.FromAccount, err = addMoney(cxt, q, arg.ToAccountID, arg.Amount, arg.FromAccountID, -arg.Amount)
-		}
-
-		return nil
-	})
-
-	return result, err
-}
-
-func addMoney(
-	cxt context.Context,
-	q *Queries,
-	accountID1 int64,
-	amount1 int64,
-	accountID2 int64,
-	amount2 int64,
-) (account1, account2 Account, err error) {
-	account1, err = q.AddAccountBalance(cxt, AddAccountBalanceParams{
-		ID:     accountID1,
-		Amount: amount1,
-	})
-	if err != nil {
-		return
-	}
-
-	account2, err = q.AddAccountBalance(cxt, AddAccountBalanceParams{
-		ID:     accountID2,
-		Amount: amount2,
-	})
-
-	return
 }
